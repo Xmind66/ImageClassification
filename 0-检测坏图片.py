cb813863ae14b39a32cef08b5c7e92b55df775e7
Xmind66 <163375024+Xmind66@users.noreply.github.com>
2024-03-18 17:36:36 +0800

    Update and rename 检测坏图片.py to 0-检测坏图片.py

--- 0-检测坏图片.py
+++ 0-检测坏图片.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 
 # 假设你的图片存放在data_dir目录下
-data_dir = r'D:\图片分类\data'
+data_dir = r'data'
 broken_files = []
 
 # 设置一个合理的像素限制，例如允许最大5000万像素的图像
@@ -15,7 +15,9 @@ for subdir, dirs, files in os.walk(data_dir):
             file_path = os.path.join(subdir, file)
             try:
                 with Image.open(file_path) as img:
-                    img.verify()  # 验证文件完整性
+                    img.verify()
+                    print(f'验证图片:{file_path}')
+                    # 验证文件完整性
             except (IOError, SyntaxError, Image.DecompressionBombError) as e:
                 print('Bad file or too large:', file_path)  # 打印损坏的文件路径或文件过大
                 broken_files.append(file_path)
@@ -23,6 +25,4 @@ for subdir, dirs, files in os.walk(data_dir):
 # 如果你想自动删除损坏的文件，可以取消注释下面的代码
 for broken_file in broken_files:
     os.remove(broken_file)
-
-# 打印损坏或者过大的文件列表
-print("List of broken or too large files:", broken_files)
+    print(f'删除不合格文件{broken_file}')
