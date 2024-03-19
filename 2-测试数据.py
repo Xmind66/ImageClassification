0cbd6ecbb766ec5c2c64b31febb6366cbf8c2e55
Xmind66 <163375024+Xmind66@users.noreply.github.com>
2024-03-19 18:51:21 +0800

    Update 2-测试数据.py

--- 2-测试数据.py
+++ 2-测试数据.py
@@ -4,12 +4,10 @@ from tensorflow.keras.preprocessing import image
 import numpy as np
 from shutil import copy2
 
-
 # 加载模型
 model = load_model('screenshot_classifier.keras')
 
 # 待检测的文件夹路径
-# source_folder = r'Data\test'
 source_folder = r'data\test'
 
 # 结果保存的文件夹路径
@@ -19,6 +17,8 @@ result_folder = r'Data\test2'
 class_a_folder = 'class_a'
 class_b_folder = 'class_b'
 
+broken_files = []
+
 # 确保结果文件夹存在
 os.makedirs(os.path.join(result_folder, class_a_folder), exist_ok=True)
 os.makedirs(os.path.join(result_folder, class_b_folder), exist_ok=True)
@@ -26,29 +26,34 @@ os.makedirs(os.path.join(result_folder, class_b_folder), exist_ok=True)
 # 遍历文件夹
 for subdir, dirs, files in os.walk(source_folder):
     for file in files:
-        # 检查文件是否为图片（这里只检查了几种常见的图片格式）
         if file.lower().endswith(('.png', '.jpg', '.jpeg')):
             img_path = os.path.join(subdir, file)
+            
+            try:
+                # 加载和预处理图片
+                img = image.load_img(img_path, target_size=(150, 150))
+                img_array = image.img_to_array(img)
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array /= 255.0
 
-            # 加载和预处理图片
-            img = image.load_img(img_path, target_size=(150, 150))
-            img_array = image.img_to_array(img)
-            img_array = np.expand_dims(img_array, axis=0)
-            img_array /= 255.0
+                # 预测图片
+                prediction = model.predict(img_array)
 
-            # 预测图片
-            prediction = model.predict(img_array)
+                # 根据预测结果保存图片
+                if prediction[0][0] > 0.5:
+                    # 图片属于 class_b
+                    destination = os.path.join(result_folder, class_b_folder, file)
+                else:
+                    # 图片属于 class_a
+                    destination = os.path.join(result_folder, class_a_folder, file)
 
-            # 根据预测结果保存图片
-            if prediction[0][0] > 0.5:
-                # 图片属于 class_b
-                destination = os.path.join(result_folder, class_b_folder, file)
-            else:
-                # 图片属于 class_a
-                destination = os.path.join(result_folder, class_a_folder, file)
+                # 复制文件到新的目录
+                copy2(img_path, destination)
+                print(f"Copied {file} to {destination}")
 
-            # 复制文件到新的目录
-            copy2(img_path, destination)
-            print(f"Copied {file} to {destination}")
+            except Exception as e:
+                broken_files.append(img_path)
+                continue
 
-print("分类完成。")
+for broken_file in broken_files:
+    print(f'处理文件异常：{broken_file}')
