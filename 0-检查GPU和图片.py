6dcb851f0dedf18000c0675a7a87aa6c525b64d1
Xmind66 <163375024+Xmind66@users.noreply.github.com>
2024-03-19 09:37:14 +0800

    Update and rename 0-检测坏图片.py to 0-检查GPU和图片.py

--- 0-检查GPU和图片.py
+++ 0-检查GPU和图片.py
@@ -1,6 +1,10 @@
+import os,send2trash
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
 from PIL import Image
-import os
 
+print("检查到可用GPU数量：", len(tf.config.experimental.list_physical_devices('GPU')))
+input('回车继续检查图片签名...')
 # 假设你的图片存放在data_dir目录下
 data_dir = r'data'
 broken_files = []
@@ -11,18 +15,19 @@ Image.MAX_IMAGE_PIXELS = 50000000
 # 遍历目录及子目录中的所有文件
 for subdir, dirs, files in os.walk(data_dir):
     for file in files:
-        if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # 确保处理PNG和JPEG文件
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')):  # 确保处理PNG和JPEG文件  
             file_path = os.path.join(subdir, file)
             try:
                 with Image.open(file_path) as img:
-                    img.verify()
+                    img.verify()    #检查图像头部签名信息
+                    image.load_img(file_path, target_size=(150, 150))   #尝试用tensorflow加载图片
                     print(f'验证图片:{file_path}')
-                    # 验证文件完整性
             except (IOError, SyntaxError, Image.DecompressionBombError) as e:
-                print('Bad file or too large:', file_path)  # 打印损坏的文件路径或文件过大
+                print('检测到坏图片:', file_path)  # 打印损坏的文件路径或文件过大
                 broken_files.append(file_path)
 
 # 如果你想自动删除损坏的文件，可以取消注释下面的代码
 for broken_file in broken_files:
-    os.remove(broken_file)
+    # os.remove(broken_file)
+    send2trash.send2trash(file_path)
     print(f'删除不合格文件{broken_file}')
